8ea8f3b645c74e8bd85447b53aaa17d48dce50c4
Camilo Prieto <prietocamilo21@gmail.com>
2024-10-20 23:49:46 -0500

    NEW -  Model
    Se agrego la interface Venta y se ajusto la clase "VentaBase", ademas de ajustes en otras clases.

--- cineProyecto/src/co/edu/konradlorenz/model/Pelicula.java
+++ cineProyecto/src/co/edu/konradlorenz/model/Pelicula.java
@@ -1,6 +1,48 @@
 package co.edu.konradlorenz.model;
 
 public class Pelicula {
-	 
-	private String Pelicula;
+
+	private String titulo;
+	private String genero;
+	private int duracion;
+
+	public Pelicula(String titulo, String genero, int duracion) {
+		this.titulo = titulo;
+		this.genero = genero;
+		this.duracion = duracion;
+	}
+
+	public Pelicula() {
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	@Override
+	public String toString() {
+		return "Pelicula [titulo=" + titulo + ", genero=" + genero + ", duracion=" + duracion + "]";
+	}
+	
+	
 }
